71cdf332289625a046a3bf67fc81a257e8087979
katarina-andrews <andrews.katarina@gmail.com>
2025-08-08 16:25:51 -0500

    imported WeatherApp

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import WeatherApp from "./components/WeatherApp";
+
 
 function App() {
-  return <></>;
+  return (
+  <>
+  <header>
+    <h1>Weather App</h1>
+    <p></p>
+  </header>
+  <main>
+    <WeatherApp />
+  </main>
+  <footer></footer>
+  </>
+  );
 }
 
 export default App;
